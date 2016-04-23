4710eeaf81e34bd35af05c34515d215c70d6a5ef
Peter Bengtsson <mail@peterbe.com>
2016-04-22 21:26:03 -0400

    0.0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 _here = path.dirname(__file__)
 
 
 setup(
     name='gg-start',
-    version='0.0.2',
+    version='0.0.3',
     description='Plugin for gg for starting branches',
     long_description=open(path.join(_here, 'README.rst')).read(),
     py_modules=['start'],
@@ -14,6 +14,8 @@ setup(
     author_email='mail@peterbe.com',
     url='https://github.com/peterbe/gg-start',
     license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=['gg'],
     entry_points="""
         [gg.plugin]
